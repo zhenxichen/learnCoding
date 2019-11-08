2f600fd4cf6fe7db212b2c5e9dc90c6c077613dd
xuanqizi <56337749+xuanqizi@users.noreply.github.com>
2019-11-08 17:56:27 +0800

    更新遍历递归表示
    更新了前序遍历、中序遍历和后序遍历的递归表示

--- DSA/BinaryTree/BinTree.cpp
+++ DSA/BinaryTree/BinTree.cpp
@@ -1,4 +1,7 @@
 #include "BinNode.h"
+#include <iostream>
+
+using namespace std; 
 
 template <typename T> class BinTree {
 protected:
@@ -8,7 +11,7 @@ protected:
 	void updateHeightAbove(BinNodePosi(T) x);					//更新x及其祖先的高度
 public:
 	int size() const { return _size; }							//const表示不允许函数修改类的成员
-	bool empty() const { return !_root };
+	bool empty() const { return !_root; }
 	BinNodePosi(T) root() { return _root; }
 };
 
@@ -25,3 +28,29 @@ template <typename T> void BinTree<T>::updateHeightAbove(BinNodePosi(T) x) {
 		x = x->parent;											//遍历其祖先
 	}
 }
+
+template <typename T> void preTrav(BinNodePosi(T) pRoot) {		//二叉树的前序遍历（递归实现）
+	if (!pRoot) { return; }
+	/*do something*/
+	cout << pRoot->data << endl;
+	
+	preTrav(pRoot->lChild);
+	preTrav(pRoot->rChild);
+}
+
+template <typename T> void inTrav(BinNodePosi(T) pRoot) {		//二叉树的中序遍历（递归实现）
+	if (!pRoot) { return; }
+	inTrav(pRoot->lChild);
+	/*do something*/
+	cout << pRoot->data << endl;
+	inTrav(pRoot->rChild);
+}
+
+template <typename T> void postTrav(BinNodePosi(T) pRoot) {		//二叉树的后续遍历（递归实现）
+	if (!pRoot) { return; }
+	postTrav(pRoot->lChild);
+	postTrav(pRoot->rChild);
+	/*do something*/
+	cout << pRoot->data << endl;
+}
+
