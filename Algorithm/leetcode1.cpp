d58c561f05fb9d25fa1942d303711387f286243f
xuanqizi <56337749+xuanqizi@users.noreply.github.com>
2020-01-30 20:00:42 +0800

    Leetcode 1 Two Sum

---
+++ Algorithm/leetcode1.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> v1;
+		for (int i = 0; i < nums.size(); i++) {
+			for (int j = i + 1; j < nums.size(); j++) {
+				if (nums[i] + nums[j] == target) {
+					v1.push_back(i);
+					v1.push_back(j);
+					return v1;
+				}
+			}
+		}
+		return v1;
+	}
+};		//暴力解：时间复杂度O(n^2)
