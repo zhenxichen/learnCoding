10a69b7ed79e6393ef38ff44051a092a440c2b17
xuanqizi <56337749+xuanqizi@users.noreply.github.com>
2020-02-10 14:37:05 +0800

    Leetcode 509 Fibonacci Number

---
+++ Algorithm/Leetcode509.cpp
@@ -0,0 +1,25 @@
+﻿/* Leetcode 509 Fibonacci Number */
+/**
+* The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+* such that each number is the sum of the two preceding ones, starting from 0 and 1.
+* Given N, calculate F(N).
+*/
+//Note: 0 ≤ N ≤ 30.
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int fib(int N) {
+		int f[31];
+		f[0] = 0;
+		f[1] = 1;
+		for (int i = 2; i <= N; i++) {
+			f[i] = f[i - 1] + f[i - 2];
+		}
+		return f[N];
+	}
+};
