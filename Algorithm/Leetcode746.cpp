463aec57f73e8d1a3fb645ac131be2cb0a93f1e0
xuanqizi <56337749+xuanqizi@users.noreply.github.com>
2020-02-10 14:27:16 +0800

    Leetcode 746 Min Cost Climbing Stairs

---
+++ Algorithm/Leetcode746.cpp
@@ -0,0 +1,27 @@
+/* Leetcode 746 Min Cost Climbing Stairs */
+/**
+* On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+* Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, 
+* and you can either start from the step with index 0, or the step with index 1.
+*/
+//Note:	1.cost will have a length in the range [2, 1000].
+//		2.Every cost[i] will be an integer in the range [0, 999].
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+		int dp[1001];				//dp[i]表示以i为终点所需的最小cost
+		int n = cost.size();
+		dp[0] = cost[0];
+		dp[1] = cost[1];
+		for (int i = 2; i < n; i++) {
+			dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+		}
+		return min(dp[n - 1], dp[n - 2]);
+	}
+};
