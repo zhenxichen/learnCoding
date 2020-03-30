9f2e097bfc7218582f0acb721352491adbba817f
xuanqizi <zhenxi_chen@foxmail.com>
2020-03-30 15:58:07 +0800

    Leetcode 182 Duplicate Emails（两种解法）

--- MySQL/Leetcode/Leetcode182.sql
+++ MySQL/Leetcode/Leetcode182.sql
@@ -1,8 +1,14 @@
 # Leetcode 182 Duplicate Emails
+# 解法一：GROUP BY + 临时表
 # Write your MySQL query statement below
-SELECT Email FROM(
-    SELECT Email, COUNT(Email) as num
-    FROM Person
-    GROUP BY Email
-) as temp
-WHERE 1<num
+#SELECT Email FROM(
+#    SELECT Email, COUNT(Email) as num
+#    FROM Person
+#    GROUP BY Email
+#) as temp
+#WHERE 1<num
+# 解法二：GROUP BY + HAVING(速度更快)
+# Write your MySQL query statement below
+SELECT Email FROM Person
+GROUP BY Email
+HAVING 1 < COUNT(Email)
