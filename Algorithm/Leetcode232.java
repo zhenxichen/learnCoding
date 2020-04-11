57d717dccf0b4f786321f2237727b69e88b2ff0e
xuanqizi <zhenxi_chen@foxmail.com>
2020-04-11 18:51:39 +0800

    Leetcode 232 用栈实现队列

---
+++ Algorithm/Leetcode232.java
@@ -0,0 +1,73 @@
+import java.util.Stack;
+
+public class Leetcode232 {
+    /* Leetcode 232 Implement Queue using Stacks */
+    /**
+     * Implement the following operations of a queue using stacks.
+     * push(x) -- Push element x to the back of queue.
+     * pop() -- Removes the element from in front of queue.
+     * peek() -- Get the front element.
+     * empty() -- Return whether the queue is empty.
+     * 说明:
+     * 你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+     * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+     * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+     */
+    //要么入队O(n), 要么出队O(n)
+    public static void main(String[] args){
+        MyQueue queue = new MyQueue();
+        queue.push(1);
+        queue.push(2);
+        queue.peek();  // returns 1
+        queue.pop();   // returns 1
+        queue.empty(); // returns false
+    }
+}
+
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        //O（n）
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        s2.push(x);
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        //O(1)
+        return s1.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        return s1.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
