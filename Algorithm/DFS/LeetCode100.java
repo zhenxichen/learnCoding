3a7fa2139fc9aa9cfdd9a76c949f7d1917f161a8
xuanqizi <1318135982@qq.com>
2020-11-13 10:36:54 +0800

    LeetCode 100 相同的树（DFS解）

---
+++ Algorithm/DFS/LeetCode100.java
@@ -0,0 +1,61 @@
+public class LeetCode100 {
+    /* LeetCode 100 相同的树 */
+    /**
+     * 给定两个二叉树，编写一个函数来检验它们是否相同。
+     * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+     */
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+
+// 解法：递归DFS
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null) {
+            return false;
+        }
+        if(p.val != q.val) {
+            return false;
+        }
+        if(!isSameTree(p.left, q.left)) {
+            return false;
+        }
+        if(!isSameTree(p.right, q.right)) {
+            return false;
+        }
+        return true;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
