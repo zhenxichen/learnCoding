6e3dd5437f21215bd971db8b1892cf7dc601b4a1
xuanqizi <1318135982@qq.com>
2020-12-16 17:11:23 +0800

    尝试实现SVM（还完全没有头绪）

--- Python/Data Mining/SVM.py
+++ Python/Data Mining/SVM.py
@@ -7,8 +7,28 @@ from sklearn.model_selection import KFold
 
 class SVM:
 	
-	def __init__(self) -> None:
+	def __init__(self, C, ) -> None:
+		self.C = C		# 参数C（见教材公式65）
+		self.b = 0		# 偏移量bias
+		self.w = []		# 权重向量
+
+
+
+	def train(self, X, Y):
 		pass
+
+	def kernal_rbf(self):
+		pass
+
+	def smo(self, X, Y):
+		b = 0
+		alphas = np.mat(np.zeros(np.shape(X)[0], 1))
+		i = 0
+		maxIter = 200	# 最大迭代次数
+		
+		self.b = b
+		return b, alphas
+
 
 def read_data():
 	filepath = 'D:\\learnCoding\\Python\\Data Mining\\heart_failure_clinical_records_dataset.csv'
