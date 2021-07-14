5e629f88d829ce203bbb592812cd6492a89961c7
zhenxichen <1318135982@qq.com>
2021-07-14 20:05:30 +0800

    LeetCode 876 链表的中间结点

---
+++ Algorithm/LinkedList/LeetCode876.java
@@ -0,0 +1,34 @@
+public class LeetCode876 {
+    /* LeetCode 876 链表的中间结点 */
+
+    /**
+     * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+     * 如果有两个中间结点，则返回第二个中间结点。
+     */
+
+    // 解法：快慢指针
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() { }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
